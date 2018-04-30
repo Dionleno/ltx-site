4c7a6ea7f13857bb86c0b08862d7e724a7eb58e4
dionleno <jhonsp_leno@hotmail.com>
2018-04-29 23:05:10 -0300

    site

---
+++ assets/js/menu.js
@@ -0,0 +1,11 @@
+
+function openNav() {
+    document.getElementById("menuMobile").style.padding = "20px";
+    document.getElementById("menuMobile").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("menuMobile").style.padding = "0px";
+    document.getElementById("menuMobile").style.width = "0";
+}
+ 
